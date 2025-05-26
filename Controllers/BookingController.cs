03b01bced73a0c8b2dc3a453cb10e54fcab5034f
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-05-26 14:24:30 +0200

    Feat(Controller): Add booking controller with GET and POST

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,43 @@
+﻿using BookingService.Data;
+using BookingService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookingController : ControllerBase
+{
+    private readonly BookingDbContext _context;
+
+    public BookingController(BookingDbContext context)
+    {
+        _context = context;
+    }
+
+    //Hämta alla boknignar
+    [HttpGet]
+    public IActionResult GetBookings()
+    {
+        var bookings = _context.Bookings.ToList();
+        return Ok(bookings);
+    }
+
+    [HttpPost]
+    public IActionResult CreateBooking([FromBody] Booking booking)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        booking.Id = Guid.NewGuid();
+        booking.BookingDate = DateTime.Now; 
+       
+
+        _context.Bookings.Add(booking);
+        _context.SaveChanges();
+
+        return Ok(booking);
+    }
+}
