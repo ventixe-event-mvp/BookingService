d4c08c29a6f0fea190a577dd7f99719a2f13662d
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-05-26 13:20:43 +0200

    Init with booking model

---
+++ Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingService.Models;
+
+public class Booking
+{
+    public Guid Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+    public  int EventId { get; set; }
+
+    public DateTime BookingDate { get; set; } = DateTime.Now;
+
+}
